19c47c5f6d58c932e6d82a8247bff4bada786214
brianach <brianocualain@outlook.ie>
2023-03-09 20:02:19 +0000

    Clean up code errors

--- run.py
+++ run.py
@@ -1,6 +1,4 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -15,22 +13,28 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize((SCOPED_CREDS))
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-sales = SHEET.worksheet('sales')
-
-data = sales.get_all_values()
 
 def get_sales_data():
     """
     Get sales figures input from the users
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+    while True:
 
-    data_str = input("Enter your data here: ")
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        data_str = input("Enter your data here: ")
+
+        sales_data = data_str.split(",")
+        validate_data(sales_data)
+
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+
+    return sales_data
+
 
 def validate_data(values):
     """
@@ -38,16 +42,17 @@ def validate_data(values):
     Raise ValueError if strings cannot be converted inot int,
     or if there aren't exactly 6 values
     """
-
     try:
         [int(value) for value in values]
-        if len(values) !=6:
+        if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
             print(f"Invalid data: {e}, please try again. \n")
+            return False
 
-get_sales_data()
+    return True
 
 
+data = get_sales_data()
